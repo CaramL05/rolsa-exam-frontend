f7724dee9c4ee9014203027a114c0f52f563bd6e
CaramL <ilja.viharevs07@gmail.com>
2025-03-25 12:01:34 +0000

    login form started

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,26 @@ import './Login.css'
 
 export default function Login(){
     return(
-        <div>
-            
+        <div className="form">
+            <form>
+                <div className="container">
+                    <label for="email">Email:</label>
+                    <input 
+                        type="email" 
+                        placeholder="Email" 
+                        name="email" 
+                        required 
+                    />
+                    <label for="password">Password:</label>
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        name="password" 
+                        required 
+                    />
+                    <button>Submit</button>
+                </div>
+            </form>
         </div>
     );
 }
