97f76e971fe477a48cbc0c952773bbe360bf63af
CaramL <ilja.viharevs07@gmail.com>
2025-03-25 12:56:55 +0000

    button

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,13 @@ import './Home.css'
 
 export default function Home(){
     return(
-        <div>
-            
+        <div className="container-parent">
+            <div className="postcode-container">
+                <form className="postcode-field">
+                    <label for="postcode"></label>
+                    <input type="text" id="text" name="text" placeholder="Enter Postcode" />
+                </form>
+            </div>
         </div>
     );
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,26 +2,18 @@ import './Login.css'
 
 export default function Login(){
     return(
-        <div className="form">
-            <form>
-                <div className="container">
+        <div className="form-parent">
+            <div className="form">
+                <form>
                     <label for="email">Email:</label>
-                    <input 
-                        type="email" 
-                        placeholder="Email" 
-                        name="email" 
-                        required 
-                    />
+                    <input type="email" id="email" name="email" placeholder="Email" />
+
                     <label for="password">Password:</label>
-                    <input 
-                        type="password" 
-                        placeholder="Password" 
-                        name="password" 
-                        required 
-                    />
-                    <button>Submit</button>
-                </div>
-            </form>
+                    <input type="password" id="password" name="password" placeholder="Password" />
+
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </div>
         </div>
     );
 }
