46e10a98f7e6bf66e0e690d40dc7f3654b24ea65
CaramL <ilja.viharevs07@gmail.com>
2025-03-25 15:44:55 +0000

    image added in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import './Home.css'
+import ChargingStation from "../../Images/Charging Station.jpg"
 
 export default function Home(){
     return(
@@ -27,6 +28,41 @@ export default function Home(){
                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </section>
+
+            <section id='section-parent'>
+                <div id='image-container'>
+                    <img src={ChargingStation}></img>
+                </div>
+                <div id='what-we-provide'>
+                    <h2>WHAT WE PROVIDE</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
+                    
+                </div>
+            </section>    
+                
+            {/* <div id='smart-home-energy'>
+                <div id='image-container'>
+
+                </div>
+
+                <p>SMART HOME ENERGY MANAGEMENT</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div> */}
+
+            
             
        </> 
     );
