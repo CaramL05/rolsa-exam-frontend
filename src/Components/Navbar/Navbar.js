e806ee1db6b55e6e9d5d8eb93b91caa0e5d0cb58
CaramL <ilja.viharevs07@gmail.com>
2025-03-25 11:43:40 +0000

    border radius and lin ks reorganised

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom"
 
 export default function Navbar(){
     return(
-        <div>
+        <div className="navbar-parent">
             <nav>
                 <ul>
+                    <li id="rolsa-logo"><Link to="/">Rolsa Technologies</Link></li>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/carbon-footprint">Carbon Footprint</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/carbon-footprint">Carbon Footprint</Link></li>
+                    <li><Link to="/contact">Help & Support</Link></li>
                     <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/sign-up">Sign Up</Link></li>
                 </ul>
             </nav>
         </div>
