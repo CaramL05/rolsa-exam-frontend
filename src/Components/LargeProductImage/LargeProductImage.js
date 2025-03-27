37541677f9c09ce697a5ed779658e7cddcc5f37f
CaramL <ilja.viharevs07@gmail.com>
2025-03-27 16:50:45 +0000

    increased font size in payment page

--- src/Components/LargeProductImage/LargeProductImage.js
+++ src/Components/LargeProductImage/LargeProductImage.js
@@ -1,15 +1,29 @@
 import './LargeProductImage.css'
 
-export default function LargeProductImage() {
+export default function LargeProductImage({description}) {
     return(
-        <div className='large-product-image-parent'>
-            <div className='product-image'>
-                
+        <div className='payment-content-page'>
+            <div className='large-product-image-parent'>
+                <div className='product-image'>
+                    Image
+                </div>
+                <div className='product-description'>
+                    <h1>*description*{description}</h1>
+                </div>
             </div>
-            <div className='product-description'>
+            <div className="payment-details-parent">
+                <div className='payment-text'>
+                    <p>Payment</p>
+                    <p>Due Today</p>
+                    <p>£999</p>
+                </div>
+                <div className='payment-buttons'>
+                    
+                </div>
+
 
             </div>
-        </div>
+       </div> 
     );
 
 }
