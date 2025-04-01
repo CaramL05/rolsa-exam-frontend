b9dc230153bf5b9ec82bcc68cb7bcde92c61c63b
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 12:05:28 +0100

    carbon footprint layout started

--- src/Pages/CarbonFootprint/CarbonFootprint.js
+++ src/Pages/CarbonFootprint/CarbonFootprint.js
@@ -3,6 +3,20 @@ import ChargingStation from "../../Images/Charging Station.jpg"
 
 export default function CarbonFootprint(){
     return(
-        <div>Carbon Footprint</div>
+        <div className="carbon-fprint-master">
+            <h1>What is a Carbon Footprint?</h1>
+            <div className="carbon-fprint-section">
+
+               <div className="placeholder-text">
+                    Lorem ipsum dolor sit amet. Ab consequatur fugit 33 sapiente dignissimos sed perferendis mollitia et vitae quibusdam! Ut vero aspernatur id odit voluptas est amet tempore cum quidem nesciunt et quaerat incidunt non quod accusamus! Ex sequi facere ut molestiae rerum et pariatur voluptatum et ducimus quae qui nobis galisum sit nemo quod! Eum tenetur labore ut Quis commodi aut galisum molestias et quae nisi nam Quis fugit ex obcaecati illo!
+                    Aut eligendi culpa rem optio eaque id animi quisquam ut Quis aliquid nam nemo sint ex exercitationem optio et quia voluptas. Et rerum sunt ut odio adipisci ea perferendis provident est fugiat accusamus et consectetur saepe ut amet explicabo. Ad labore quam cum quae tempora aut eligendi explicabo.
+                    Ut dolore nostrum sit atque soluta ea pariatur earum ut delectus dolorum aut voluptatum libero. Qui debitis quos vel reiciendis blanditiis qui porro totam sed earum consequatur et neque fugit in aperiam maiores et atque sequi. Et natus quisquam sit voluptatum iusto qui doloremque perspiciatis. Sit velit voluptatem vel galisum possimus et voluptatem dicta qui dolorum suscipit aut totam perferendis.
+                </div> 
+                <div className="carbon-fprint-image">
+                    Image
+                </div>
+            </div>
+
+        </div>
     );
 }
