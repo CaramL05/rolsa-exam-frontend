e5c8341b36a88845df805f0bff7dda0cb7c4923d
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 16:50:23 +0100

    register button added to login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,24 @@
 import './Login.css'
+import { Link } from 'react-router-dom';
 
-export default function Login(){
-    return(
-        <div className="form-parent">
-            <div className="form">
-                <form>
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Email" />
+export default function Login() {
+    return (
+        <div className='form-master'>
+            <h1>Login</h1>
+            <div className="form-parent">
+                
+                <div className="form">
+                    <form>
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="Email" />
 
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Password" />
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" placeholder="Password" />
 
-                    <input type="submit" value="Submit"></input>
-                </form>
+                        <input type="submit" value="Submit"></input>
+                        <button type="submit" value="Register"><Link to="#">Register</Link></button>
+                    </form>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,5 +1,6 @@
 import './Result.css'
 import GreenFoot from "../../Images/GreenFoot.png"
+import { Link } from 'react-router-dom';
 
 export default function Result(){
     return(
@@ -16,6 +17,19 @@ export default function Result(){
                 </div>    
             </div>
 
+            <div className='key-container-parent'>
+                <div className='result-information'>
+                    <p>Over 5 : your carbon footprint is high. Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes penatibus molestie volutpat inceptos aenean viverra consequat. </p>
+                    <p>Between 2 and 5 : your carbon footprint relatively low. Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes penatibus molestie volutpat inceptos aenean viverra consequat. </p>
+                    <p>Under 2 : your carbon footprint low. Lorem ipsum odor amet, consectetuer adipiscing elit. Leo montes penatibus molestie volutpat inceptos aenean viverra consequat. </p>
+                </div>
+            </div>
+
+            <div className='save-score-btn'>
+                <div className='service-btn'>
+                    <button><Link to="/login">Save Score</Link></button>
+                </div>    
+            </div>
             
             
 
