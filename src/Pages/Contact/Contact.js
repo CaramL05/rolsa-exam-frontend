493968e3415ce69400641869403d0b6241c809ae
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 14:32:08 +0100

    enquiry finished

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -8,9 +8,48 @@ export default function Contact(){
                 <input 
                     placeholder="Name"
                     type="text"
-                    
+                    id="name"
+                    name="name"
                 />
+
+                <div className="support-email">
+                    <input 
+                        placeholder="Email"
+                        type="text"
+                        id="name"
+                        name="name"
+                    /> 
+                </div>
+                
+
+            <div className='enquiry-dropdown'>
+                <select id="enquiry-type">
+                    
+                    <option value="lorem">Enquiry Type:</option>
+                    <option value="lorem">Enquiry type 1</option>
+                    <option value="lorem">Enquiry type 2</option>
+                    <option value="lorem">Enquiry type 3</option>
+                    <option value="lorem">Enquiry type 4</option>
+                </select>
             </div>
+
+                <input 
+                    className="enquiry-field"
+                    placeholder="Enquiry"
+                    type="text"
+                    id="enquiry"
+                    name="enquiry"
+                />
+
+            </div>
+            
+            <div className='support-submit-btn-parent'>
+                <div className="support-submit-btn">
+                    <button>Submit</button>  
+                </div>    
+            </div>
+            
+            
         </div>
     );
 }
