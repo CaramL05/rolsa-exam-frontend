742529e1a31227e0aec72cece4247f520213449d
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 12:47:06 +0100

    help and support page started

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,8 +2,15 @@ import './Contact.css'
 
 export default function Contact(){
     return(
-        <div>
-            
+        <div className='support-master'>
+            <h1>Help & Support</h1>
+            <div className="support-input-container">
+                <input 
+                    placeholder="Name"
+                    type="text"
+                    
+                />
+            </div>
         </div>
     );
 }
