bfb5eab4b28960c6d53b4bd983fd206568de69d9
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 11:26:59 +0100

    more containers added to shop page

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -7,6 +7,12 @@ export default function Shop(){
         <h1>Shop</h1>
         <div className='shop-container'>
             <ProductContainer  description="product 1" buttontxt="Add to cart" price="£59.99"/>
+            <ProductContainer  description="product 2" buttontxt="Add to cart" price="£59.99"/>
+            <ProductContainer  description="product 3" buttontxt="Add to cart" price="£59.99"/>
+            
+            <ProductContainer  description="product 4" buttontxt="Add to cart" price="£59.99"/>
+            <ProductContainer  description="product 5" buttontxt="Add to cart" price="£59.99"/>
+            <ProductContainer  description="product 6" buttontxt="Add to cart" price="£59.99"/>
 
         </div>
         </div>
