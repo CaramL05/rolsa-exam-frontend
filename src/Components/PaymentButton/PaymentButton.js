0c78355f690564874d820eb6e966433b513ae77f
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 11:09:51 +0100

    color issue fixed

--- src/Components/PaymentButton/PaymentButton.js
+++ src/Components/PaymentButton/PaymentButton.js
@@ -2,6 +2,8 @@ import './PaymentButton.css'
 
 export default function PaymentButton({text}) {
     return(
-        <button>{text}</button>
+        <div className='payment-btn'>
+            <button>{text}</button>
+        </div>
     );
 }
