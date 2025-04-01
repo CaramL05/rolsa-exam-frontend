6d5057ef0c9e12af89ace41be1864f0b5bcae3dd
CaramL <ilja.viharevs07@gmail.com>
2025-04-01 11:24:06 +0100

    Reusing service containers to the shop page

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,9 +1,14 @@
 import './Shop.css'
+import ProductContainer from '../../Components/ProductContainer/ProductContainer';
 
 export default function Shop(){
     return(
-        <div>
-            
+        <div className='shop-container-master'>
+        <h1>Shop</h1>
+        <div className='shop-container'>
+            <ProductContainer  description="product 1" buttontxt="Add to cart" price="£59.99"/>
+
+        </div>
         </div>
     );
 }
