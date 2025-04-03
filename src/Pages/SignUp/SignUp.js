f5450ce13c3fccb3d96f54f71943c5ddd478eee1
CaramL <ilja.viharevs07@gmail.com>
2025-04-03 11:04:26 +0100

    Login and SignUp forms completed

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -3,11 +3,17 @@ import './SignUp.css'
 export default function SignUp(){
     return(
         <div className='sign-up-master'>
+            <h1>Sign Up</h1>
             <div className="form-parent">
                 <div className="form">
                     <form>
 
-                        
+                        <label for="fname">Full name:</label>
+                        <input type="text" id="fname" name="fname" placeholder="First name" />
+
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username" placeholder="Username" />
+
                         <label for="email">Email:</label>
                         <input type="email" id="email" name="email" placeholder="Email" />
 
